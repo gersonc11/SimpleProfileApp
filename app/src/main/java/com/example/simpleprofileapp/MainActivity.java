089bd8c0f79b3376d04f45a5ec91892eb243edab
gersonc11 <gcruz1776@gmail.com>
2019-07-01 10:56:39 -0400

    created profile page

--- app/src/main/java/com/example/simpleprofileapp/MainActivity.java
+++ app/src/main/java/com/example/simpleprofileapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.simpleprofileapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -15,7 +17,15 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton submit;
     TextInputEditText name, username, password, description;
     String nameString, userString, passString, descString;
-    boolean created = false;
+
+    public static final String MyPREFERENCES = "MyPrefsFile";
+    public static final String typeName = "namekey";
+    public static final String typeUser = "userkey";
+    public static final String typePass = "passkey";
+    public static final String typeDesc = "desckey";
+
+    SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +33,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         submit = findViewById(R.id.submit_button);
 
+        name = findViewById(R.id.name_field);
+        username = findViewById(R.id.user_field);
+        password = findViewById(R.id.password_field);
+        description = findViewById(R.id.description_field);
 
+        sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        String oldName = sharedPreferences.getString(typeName, nameString);
+        String oldUser = sharedPreferences.getString(typeUser, userString);
+        String oldPass = sharedPreferences.getString(typePass, passString);
+        String oldDesc = sharedPreferences.getString(typeDesc, descString);
+
+        name.setText(oldName);
+        username.setText(oldUser);
+        password.setText(oldPass);
+        description.setText(oldDesc);
     }
 
     @Override
     protected void onStart() {
+
         super.onStart();
+
     }
 
     @Override
@@ -38,10 +64,6 @@ public class MainActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                name = findViewById(R.id.name_field);
-                username = findViewById(R.id.user_field);
-                password = findViewById(R.id.password_field);
-                description = findViewById(R.id.description_field);
 
                 nameString = name.getText().toString();
                 userString = username.getText().toString();
@@ -49,17 +71,34 @@ public class MainActivity extends AppCompatActivity {
                 descString = description.getText().toString();
 
 
-
-                Toast.makeText(MainActivity.this, "jj" + nameString + userString + passString + descString, Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(getBaseContext(), ProfileCard.class);
                 intent.putExtra("name", nameString);
                 intent.putExtra("username", userString);
                 intent.putExtra("password", passString);
                 intent.putExtra("description", descString);
                 startActivity(intent);
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("namekey", nameString);
+                editor.putString("userkey", userString);
+                editor.putString("passkey", passString);
+                editor.putString("desckey", descString);
+                editor.commit();
             }
         });
+
     }
+
+    @Override public void onSaveInstanceState(Bundle outState) {
+
+        outState.putString("name_key", nameString);
+        outState.putString("username_key", userString);
+        outState.putString("password_key", passString);
+        outState.putString("description_key", descString);
+
+        super.onSaveInstanceState(outState);
+    }
+
 
     @Override
     protected void onPause() {
--- app/src/main/java/com/example/simpleprofileapp/ProfileCard.java
+++ app/src/main/java/com/example/simpleprofileapp/ProfileCard.java
@@ -2,10 +2,18 @@ package com.example.simpleprofileapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileCard extends AppCompatActivity {
+
+    ImageView backButton;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,21 @@ public class ProfileCard extends AppCompatActivity {
         String password = intent.getStringExtra("password");
         String description = intent.getStringExtra("description");
 
+        Toast.makeText(ProfileCard.this, "wow" , Toast.LENGTH_SHORT).show();
+
+        textView = findViewById(R.id.text_view);
+        backButton = findViewById(R.id.back_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+            });
+
+
+        textView.setText("Name " + name + "\n User Name: "+ user + "\n Password" + password + "\n Description: " + description);
     }
 
     @Override
